c8a3f12e03d66a6b48076e3dfc871829e5c8496c
Daniel Souza <workdndsza@gmail.com>
2025-03-06 17:13:38 -0300

    start projeto

---
+++ src/test/java/com/dnsouzadev/canesfacil/CanesFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.canesfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanesFacilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
